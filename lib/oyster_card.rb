48c036b1fad7a6c23fe71e3f27347d8c9e92e80e
Nahayo Nowomuhangi <christine_nn@live.co.uk>
2019-02-11 15:09:44 +0000

    step 6 = adding a limit as a constant

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,9 +1,16 @@
 class Oystercard
+  LIMIT = 90 # add limit as a constant
   attr_reader :balance
+  attr_reader :limit
   def initialize
     @balance = 0
+    @limit = LIMIT
   end
   def top_up(value)
+    if (@balance + value) > @limit
+      raise "maxium top up is #{@limit}" # use this to incorporate code inside string.
+    else
     @balance = @balance + value
+    end
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,7 +7,11 @@ describe Oystercard do
 
   it "top's up the balance" do
     subject.top_up(5)
-    expect(subject.balance).to eq 5
+    expect(subject.balance).to eq 5 # . balance is accessing attr_reader
   end
+
+it "raises error if amount is beyond limit" do
+  expect { subject.top_up(91) }.to raise_error "maxium top up is #{subject.limit}" #limit is accessing attr_reader
+end
 
 end
