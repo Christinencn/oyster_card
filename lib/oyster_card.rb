fde1874a34a362bcd2f5afe4744eb3ff4d293695
Nahayo Nowomuhangi <christine_nn@live.co.uk>
2019-02-14 11:28:51 +0000

    step 8 - touch in and out

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,10 +1,12 @@
 class Oystercard
-  LIMIT = 90 # add limit as a constant
-  attr_reader :balance # attr makes these variables accessible in spec
-  attr_reader :limit
+  LIMIT = 90 # add limit as a constant so it stays the same
+  attr_reader :balance # attr_reader makes these variables accessible in spec
+  attr_reader :limit, :in_journey
+
   def initialize
     @balance = 0
     @limit = LIMIT
+    @in_journey = false
   end
 
   def top_up(value)
@@ -16,6 +18,18 @@ class Oystercard
   end
 
   def deduct(fare)
-    @balance = @balance - fare 
+    @balance = @balance - fare
+  end
+
+  def in_journey?
+    false
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -10,15 +10,28 @@ describe Oystercard do
     expect(subject.balance).to eq 5 # . balance is accessing attr_reader
   end
 
-it "raises error if amount is beyond limit" do
+  it "raises error if amount is beyond limit" do
   expect { subject.top_up(91) }.to raise_error "maxium top up is #{subject.limit}" #uses {} syntax as it is raising an error, limit is accessing attr_reader
-end
+  end
 
-it "reduces balance when used" do
+  it "reduces balance when used" do
   subject.top_up(5) # need to top up first inorder to increase the balance
   subject.deduct(2)
   expect(subject.balance).to eq 3
+  end
 
-end
+  it "touch in at the start of your journey" do
+  subject.touch_in
+expect(subject.in_journey).to eq true
+  end
 
+  it "is in the journey" do
+  expect(subject.in_journey).to eq false
+  end
+
+  it "can touch out " do
+    subject.touch_in
+    subject.touch_out
+    expect(subject.in_journey).to eq false
+  end
 end
